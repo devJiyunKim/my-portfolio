c7ca91fc8c13ce7b3122db4d488bb8af9e42aa06
Jiyun Kim <51939481+devJiyunKim@users.noreply.github.com>
2022-10-15 00:35:53 +0900

    반응형 (Media query) 추가

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-var back = document.querySelectorAll('#menuBar #back');
-
 function menuControl(menuOpen, menuClose_one, menuClose_two, menuClose_three)
 {
-  var menuOpen= document.getElementById(menuOpen);
-  menuOpen.style.display = "block";
-  menuOpen.childNodes[1].childNodes[1].style.display = 'block';
+  var menu = document.getElementById(menuOpen);
+  menu.childNodes[1].childNodes[1].style.display = 'block';
+  menu.style.display = "block";
   document.getElementById(menuClose_one).style.display = "none";
   document.getElementById(menuClose_two).style.display = "none";
   document.getElementById(menuClose_three).style.display = "none";
