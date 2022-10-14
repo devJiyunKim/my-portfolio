c427d00b8f71a187e5976bea2d976a34a032f1ee
Jiyun Kim <51939481+devJiyunKim@users.noreply.github.com>
2022-10-14 23:27:50 +0900

    전체 리뉴얼

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
+var back = document.querySelectorAll('#menuBar #back');
+
 function menuControl(menuOpen, menuClose_one, menuClose_two, menuClose_three)
 {
-  document.getElementById('back').style.display = 'block';
-  document.getElementById(menuOpen).style.display = "block";
+  var menuOpen= document.getElementById(menuOpen);
+  menuOpen.style.display = "block";
+  menuOpen.childNodes[1].childNodes[1].style.display = 'block';
   document.getElementById(menuClose_one).style.display = "none";
   document.getElementById(menuClose_two).style.display = "none";
   document.getElementById(menuClose_three).style.display = "none";
-  document.getElementById('greeting').style.display = "none";
-  document.getElementById('category').style.display = "none";
+  document.getElementById('main').style.display = "none";
 }
